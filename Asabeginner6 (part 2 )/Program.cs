3357a10eae9e306cb5f6304f8ecf69360fa75cd7
iampr <iampr@PRABIN>
2023-01-03 21:51:01 +0545

    Add project files.

---
+++ Asabeginner6 (part 2 )/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int? TicketOnSale = 100;
+        int AvailableTicket = TicketOnSale ?? 0;
+        Console.WriteLine(AvailableTicket);
+         
+    }
+}
